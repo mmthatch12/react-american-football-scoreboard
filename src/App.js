b1f507c9f9561869ada9c5ff2b9f027f11442616
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-15 13:29:19 -0600

    working on step 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const[firstValue, secondValue] = useState();
 
   return (
     <div className="container">
