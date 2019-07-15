31d66988e3b4ea371adcb4877794776cb1114626
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-15 13:57:06 -0600

    working on step 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const[firstValue, secondValue] = useState(0);
+  const[currScore, addPoints] = useState(0);
 
   return (
     <div className="container">
@@ -16,12 +16,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{firstValue}</div>
+            <div className="home__score">{currScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{firstValue}</div>
+            <div className="away__score">{currScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -29,8 +29,9 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+
+          <button className="homeButtons__touchdown" onClick={() => addPoints(currScore + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => addPoints(currScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown">Away Touchdown</button>
